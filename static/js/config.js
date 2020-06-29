59a21f4e222580f804409bcc06e22aea986214f9
Tajahnei <57681396+tajahnei@users.noreply.github.com>
2020-06-29 13:55:43 -0400

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
 // API key
 // TODO: Insert your own API key here
-const API_KEY = "pk.eyJ1IjoidGFqYWhuZWkiLCJhIjoiY2thdHBtbzYyMXhtdDJ4cDEzdndld2IzMSJ9.kW1RZskqRZgy_4sizKAoIg";
+const API_KEY = "Your API Key Here";
